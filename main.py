54a8f01a9ea92f8af1836bf8e6b9f43544483dd0
Abirokaya <rokayaabinash9@gmail.com>
2026-08-08 09:57:47 +0545

    abfakjdsa

--- main.py
+++ main.py
@@ -1,38 +1,64 @@
 from fastapi import FastAPI
-import uvicorn
-from database.db import Base, engine
-from route import (user_route,admin_route,birth_registration_route, ward_route,ward_secretary_route,ward_chairperson_route,citizen_route,notice_route,certificate_router,
-                   deat_registration_route,migration_registration_route,recommendation_router,complaint_route,data_validation_route,analytics_router,admin_analytics_router,                  tax_router)
-
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
 
+from database.db import Base, engine
+
+from route import (
+    user_route,
+    admin_route,
+    birth_registration_route,
+    ward_route,
+    ward_secretary_route,
+    ward_chairperson_route,
+    citizen_route,
+    notice_route,
+    certificate_router,
+    deat_registration_route,
+    migration_registration_route,
+    recommendation_router,
+    complaint_route,
+    data_validation_route,
+    analytics_router,
+    admin_analytics_router,
+    tax_router,
+)
 
 app = FastAPI()
+
 origins = [
     "http://localhost:5173",
-"https://e-ward-frontend.vercel.app"]
-os.makedirs("static/wards", exist_ok=True)
-app.mount("/static", StaticFiles(directory="static"), name="static")
-# Base.metadata.create_all(bind=engine)
+    "https://e-ward-frontend.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-Base.metadata.create_all(bind=engine) 
+os.makedirs("static/wards", exist_ok=True)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def read_root():
-    return {"message":"Hello World"}
+    return {"message": "Hello World"}
 
 
 app.include_router(user_route.router)
-app.include_router(admin_route.router)    
+app.include_router(admin_route.router)
 app.include_router(birth_registration_route.router)
 app.include_router(ward_secretary_route.router)
 app.include_router(ward_chairperson_route.router)
@@ -50,6 +76,12 @@ app.include_router(tax_router.router)
 app.include_router(admin_analytics_router.router)
 
 
-if __name__=="__main__":
-    uvicorn.run("main:app",host="localhost",port=8000,reload=True)
-    
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8000,
+        reload=True
+    )
